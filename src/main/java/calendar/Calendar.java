d4286d12248eceb4b31b7d15a840e9204fd07fc0
Istvan Viczian <viczian.istvan@gmail.com>
2022-04-19 16:28:25 +0200

    Kivonás

--- src/main/java/calendar/Calendar.java
+++ src/main/java/calendar/Calendar.java
@@ -1,10 +1,14 @@
 package calendar;
 
-import java.util.List;
+//import java.util.List;
 
 public class Calendar {
 
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
